012be8bfd9cbe17a22940a52513f4fc450a45a54
20172105788 <39482047+20172105788@users.noreply.github.com>
2018-06-11 16:29:20 +0800

    Initial commit

---
+++ program/program/program.cpp
@@ -0,0 +1,56 @@
+// program.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+class CFeet
+{
+protected:
+	int feet;
+	int inches;
+public:
+	CFeet();
+	void setfeet(int f);
+	void setinches(int ich);
+	void display();
+	int getfeet();
+	int getinches();
+};
+CFeet::CFeet()
+{
+	feet = 0;
+	inches = 0;
+}
+void CFeet::setfeet(int f)
+{
+	feet = f;
+}
+void CFeet::setinches(int ich)
+{
+	inches = ich % 12;
+	feet = feet + ich / 12;
+}
+void CFeet::display()
+{
+	cout << feet << "英尺" << inches << "英寸" << endl;
+}
+int CFeet::getfeet()
+{
+	return feet;
+}
+int CFeet::getinches()
+{
+	return inches;
+}
+
+
+int main()
+{
+	CFeet Afeet;
+	Afeet.setfeet(3);
+	Afeet.setinches(8);
+	Afeet.display();
+	return 0;
+    return 0;
+}
